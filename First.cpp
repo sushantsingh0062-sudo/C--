cbf8e4d2e6b65dd98abb75c0918cb0462a98b5bb
sushantsingh0062-sudo <sushantsingh0062@gmail.com>
2026-01-10 20:14:58 +0530

    first commit

---
+++ First.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;
+int main () { 
+    int a, b, sum;
+    cout <<"Enter two numbers: ";
+    cin >> a;
+    
+    cout << "Please enter second number: ";
+   cin >> b;
+
+    sum = a + b;
+    cout << "The sum is: " << sum << endl;
+
+}
+
+
+
